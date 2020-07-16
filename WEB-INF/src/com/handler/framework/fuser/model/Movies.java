43eaf301eeee7d709a3eb62ced39962d4be19144
k-shivam03 <shivam311kanungo@gmail.com>
2020-07-16 15:55:14 +0530

    Handler Framework User .class and .java files

---
+++ WEB-INF/src/com/handler/framework/fuser/model/Movies.java
@@ -0,0 +1,12 @@
+package com.handler.framework.fuser.model;
+
+public class Movies {
+    private MovieWrapper[] movieArr;
+
+    public void setMovieArr(MovieWrapper[] movieArr) {
+        this.movieArr = movieArr;
+    }
+    public MovieWrapper[] getMovieArr() {
+        return this.movieArr;
+    }
+}
